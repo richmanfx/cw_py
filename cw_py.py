e7228c2672c9bcbb2c6ba38d799b649ec556033f
Jashhuk Aleksandr <richmanfx@yandex.ru>
2016-05-21 12:52:15 +0300

    Added Checking of valid values for command line arguments.

--- cw_py.py
+++ cw_py.py
@@ -6,6 +6,8 @@ import sys
 import argparse
 # import random
 
+version = '1.0.0'
+
 __author__ = 'Aleksandr Jashhuk, Zoer, R5AM, www.r5am.ru'
 
 
@@ -21,8 +23,9 @@ def clear_console():
 def create_parser(default_cw_file):
     parser = argparse.ArgumentParser(description='Generation of random CW words.',
                                      epilog='For example: cw_py.py -n 50 -t 750 -s 100 -p 3')
-    parser.add_argument('--version', action='version', version='cw_py 1.0.0')
-    parser.add_argument('--file', '-f', default=default_cw_file, help='file with CW words')
+    parser.add_argument('--version', '-v', action='version', version='%(prog)s {}'.format(version))
+    parser.add_argument('file', nargs='?', type=argparse.FileType(),
+                        default=default_cw_file, help='file with CW words')
     parser.add_argument('--num', '-n', type=int, default=50, help='quantity of CW words')
     parser.add_argument('--pause', '-p', type=int, default=1, help='pause between words (in the dash)')
     parser.add_argument('--speed', '-s', type=int, default=100, help='speed (characters per minute)')
@@ -30,15 +33,41 @@ def create_parser(default_cw_file):
     return parser
 
 
+# Провка диапазона допустимых значений
+def valid_range(minimum, maximum, variable):
+    if (variable < minimum) or (variable > maximum):
+        result = False
+    else:
+        result = True
+    return result
+
+
 def main():
-    default_cw_file = 'cw.txt'                              # Файл по-умолчанию с CW словами
+    default_cw_file = 'cw.txt'  # Файл по-умолчанию с CW словами
 
     # Очистить консоль
     clear_console()
     # Обработать аргументы командной строки
-    parser = create_parser(default_cw_file)                 # Экземпляр парсера
+    parser = create_parser(default_cw_file)  # Экземпляр парсера
     namespace = parser.parse_args()
-    cw_name = namespace.file                                # имя CW файла из командной строки
+    cw_name = namespace.file  # файл из командной строки с CW словами
+
+    # Проверка допустимых значений аргументов командной строки
+    # bad_argument
+    if not valid_range(1, 1000, namespace.num):
+        print('Недопустимое значение для количества слов (1...1000).')
+        sys.exit()  # Выход из программы
+    if not valid_range(1, 15, namespace.pause):
+        print('Недопустимое значение для паузы между словами (1...15).')
+        sys.exit()
+    if not valid_range(50, 250, namespace.speed):
+        print('Недопустимое значение для скорости (50...250).')
+        sys.exit()
+    if not valid_range(300, 2000, namespace.tone):
+        print('Недопустимое значение для тона (300...2000).')
+        sys.exit()
+
+    print(namespace)
 
 
 if __name__ == '__main__':
